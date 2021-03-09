e4470c172800a37b4a3104f0df5e71db4e903e04
timja <tjmccabe@email.neit.edu>
2021-03-08 21:37:39 -0500

    CRUD Functionality

---
+++ BlockBuster/BlockBusterAdminFunctions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using BlockBuster.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BlockBuster
+{
+    public class BlockBusterAdminFunctions
+    {
+        public static void AddMovie(Movie movie)
+        {
+            try
+            {
+                using (var db = new SE407_BlockBusterContext())
+                {
+                    db.Movies.Add(movie);
+                    db.SaveChanges();
+                }
+            } catch (Exception e)
+            {
+
+            }
+        }
+        public static void DeleteMovie(int id)
+        {
+            try
+            {
+                using (var db = new SE407_BlockBusterContext())
+                {
+                    var movieToDelete = db.Movies.Find(id);
+                    db.Movies.Remove(movieToDelete);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+        public static void EditMovie(Movie movie)
+        {
+            try
+            {
+                using (var db = new SE407_BlockBusterContext())
+                {
+                    db.Movies.Update(movie);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
